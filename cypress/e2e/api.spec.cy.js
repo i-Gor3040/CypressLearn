58bd4fc5471dc1fb31d4efca1ce9ed082be0eba5
IgorPharmacist <igorshchuk@digitalpharmacist.com>
2023-10-24 15:24:06 +0300

    mock tags

--- cypress/e2e/api.spec.cy.js
+++ cypress/e2e/api.spec.cy.js
@@ -1,10 +1,12 @@
 /// <reference types="cypress" />
 import { onLandingPage } from "../support/pageObject/landingPage";
 
-let NAME;
-let METHOD;
-let URL;
-let STATUSCODE;
+let name;
+let method;
+let url;
+let statusCode;
+// let ARR;
+let obj;
 
 describe("API intercept", () => {
   beforeEach("open app", () => {
@@ -12,37 +14,53 @@ describe("API intercept", () => {
     onLandingPage.openBasePage();
   });
 
-  it("Tags request", () => {
+
+  it("Tags mock", () => {
+    name = "tags";
+    method = "GET";
+    url = "https://api.realworld.io/api/tags";
+    obj = {
+      "tags": [
+        "Warsaw",
+        "Krakow",
+        "Lodz",
+        "Wroclaw",
+        "Gdansk",
+        "Szczecin",
+        "Bydgoszcz",
+        "Lublin ",
+        "Białystok",
+      ],
+    }
+    statusCode = 202;
+
+    onLandingPage.interceptAndMockCall(name, method, url, statusCode, obj)
+  });
+
+ /*  it("Articles mock", () => {
+    NAME = "articles";
+    METHOD = "GET";
+    URL = "https://api.realworld.io/api/articles?limit=10&offset=0";
+    STATUSCODE = 200;
+
+    onLandingPage.interceptAndMockCall(NAME, METHOD, URL, STATUSCODE)
+  }); */
+ /*  it("Tags request", () => {
     NAME = "Popular Tags";
     METHOD = "GET";
     URL = "https://api.realworld.io/api/tags";
     STATUSCODE = 200;
 
     onLandingPage.interceptCall(NAME, METHOD, URL, STATUSCODE)
-  });
+  }); */
 
-  it("Articles request", () => {
+ /*  it("Articles request", () => {
     NAME = "Articles";
     METHOD = "GET";
     URL = "https://api.realworld.io/api/articles?limit=10&offset=0";
     STATUSCODE = 200;
 
     onLandingPage.interceptCall(NAME, METHOD, URL, STATUSCODE)
-  });
-
-  /* it("Tags request", () => {
-    cy.intercept({
-      method: "GET",
-      url: "https://api.realworld.io/api/tags",
-    }).as("Popular Tags");
-    cy.wait("@Popular Tags").its("response.statusCode").should("eq", 200);
-  });
-
-  it("Articles request", () => {
-    cy.intercept({
-      method: "GET",
-      url: "https://api.realworld.io/api/articles?limit=10&offset=0",
-    }).as("Articles");
-    cy.wait("@Articles").its("response.statusCode").should("eq", 200);
   }); */
+
 });
--- cypress/support/pageObject/landingPage.js
+++ cypress/support/pageObject/landingPage.js
@@ -1,10 +1,28 @@
 export class LandingPage {
-  interceptCall(NAME, METHOD, URL, STATUSCODE) {
+  /* interceptCall(NAME, METHOD, URL, STATUSCODE) {
     cy.intercept({
       method: METHOD, // "GET"
       url: URL, // "https://api.realworld.io/api/tags"
     }).as(NAME); // "Popular Tags"
-    cy.wait(`@${NAME}`).its("response.statusCode").should("eq", STATUSCODE);
+    cy.wait(`@${NAME}`).then(({response}) => {
+      console.log(response.body)
+      expect(response.statusCode).to.eq(STATUSCODE)
+      expect(response.body).to.have.property("tags")
+      console.log(response.body.tags)
+    })
+  }
+ */
+
+  interceptAndMockCall(name, method, url, statusCode, obj) {
+    cy.intercept({
+      method: method,
+      url: url},
+      (req) => {
+      req.reply({
+        statusCode: statusCode,
+        body: obj,
+      });
+    }).as(name);
   }
 
   openBasePage() {
